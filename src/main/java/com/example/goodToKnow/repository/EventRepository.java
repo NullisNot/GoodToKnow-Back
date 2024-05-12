1793e53865c1b117ac2d67b922fda57fed618882
Carlos ALvarez <carlosmail1978@gmail.com>
2024-05-12 11:26:13 +0200

    Add event controller with get event by day method, event repository and event service

---
+++ src/main/java/com/example/goodToKnow/repository/EventRepository.java
@@ -0,0 +1,13 @@
+package com.example.goodToKnow.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.goodToKnow.entity.Event;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+  List<Event> findByStartsAtBetween(LocalDateTime start, LocalDateTime end);
+
+}
