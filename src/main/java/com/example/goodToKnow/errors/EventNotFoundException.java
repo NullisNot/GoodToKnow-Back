81449461e84e04f7fab4002124eeaa0311fd2edd
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-18 17:27:47 +0200

    Fix eventNotFound exception

--- src/main/java/com/example/goodToKnow/errors/EventNotFoundException.java
+++ src/main/java/com/example/goodToKnow/errors/EventNotFoundException.java
@@ -1,5 +1,7 @@
 package com.example.goodToKnow.errors;
 
 public class EventNotFoundException extends RuntimeException {
-
+  public EventNotFoundException() {
+    super("Event not found");
+  }
 }
