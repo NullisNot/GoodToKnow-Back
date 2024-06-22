fb0708c6560b9ccf64566fce66178202cb281bb8
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-22 15:34:13 +0200

    Update cors

--- src/main/java/com/example/goodToKnow/auth/AuthController.java
+++ src/main/java/com/example/goodToKnow/auth/AuthController.java
@@ -12,17 +12,18 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = { "http://localhost:4200" })
 public class AuthController {
 
   private final AuthService authService;
 
   @PostMapping(value = "login")
+  @CrossOrigin(origins = "http://localhost:4200")
   public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
     return ResponseEntity.ok(authService.login(request));
   }
 
   @PostMapping(value = "register")
+  @CrossOrigin(origins = "http://localhost:4200")
   public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
     return ResponseEntity.ok(authService.register(request));
   }
