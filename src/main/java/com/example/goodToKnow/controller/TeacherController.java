c136133496793520622a5efae1a138f0eee89d24
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-28 14:12:02 +0200

    Fix import in teacher controller

--- src/main/java/com/example/goodToKnow/controller/TeacherController.java
+++ src/main/java/com/example/goodToKnow/controller/TeacherController.java
@@ -8,14 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.goodToKnow.entity.Teacher;
 import com.example.goodToKnow.service.TeacherService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController()
 @RequestMapping(path = "api/v1/teachers")
