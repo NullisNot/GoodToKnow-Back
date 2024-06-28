f759b385b5cea9ee5194da9d9de08183c51b07de
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-28 14:25:07 +0200

    Update classroom controller

--- src/main/java/com/example/goodToKnow/controller/ClassroomController.java
+++ src/main/java/com/example/goodToKnow/controller/ClassroomController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +17,21 @@ import com.example.goodToKnow.service.ClassroomService;
 
 @RestController()
 @RequestMapping(path = "api/v1/classrooms")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class ClassroomController {
   @Autowired
   private ClassroomService classroomService;
 
-  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Classroom> getClassrooms() {
     return classroomService.getClassrooms();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Classroom createClassroom(@RequestBody Classroom classroom) {
+    return classroomService.saveClassroom(classroom);
   }
 
 }
