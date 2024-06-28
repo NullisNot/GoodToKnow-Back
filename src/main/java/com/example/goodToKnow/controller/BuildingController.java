a0a91015ed8bd2d8a2ebba66cdc9be788b9db32a
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-28 14:30:08 +0200

    Update building controller

--- src/main/java/com/example/goodToKnow/controller/BuildingController.java
+++ src/main/java/com/example/goodToKnow/controller/BuildingController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,15 +17,21 @@ import com.example.goodToKnow.service.BuildingService;
 
 @RestController()
 @RequestMapping(path = "api/v1/buildings")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class BuildingController {
   @Autowired
   private BuildingService buildingService;
 
-  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Building> getBuildings() {
     return buildingService.findBuildings();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Building createBuilding(@RequestBody Building building) {
+    return buildingService.saveBuilding(building);
   }
 
 }
