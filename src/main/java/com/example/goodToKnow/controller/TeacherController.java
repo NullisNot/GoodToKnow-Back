1452523c54f08871c4fcc1e9deafba658900b31d
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-28 12:55:42 +0200

    Update teacher controller

--- src/main/java/com/example/goodToKnow/controller/TeacherController.java
+++ src/main/java/com/example/goodToKnow/controller/TeacherController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.goodToKnow.entity.Teacher;
 import com.example.goodToKnow.service.TeacherService;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController()
 @RequestMapping(path = "api/v1/teachers")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class TeacherController {
   @Autowired
   private TeacherService teacherService;
 
-  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Teacher> getTeachers() {
     return teacherService.findTeachers();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Teacher createTeacher(@RequestBody Teacher teacher) {
+    return teacherService.saveTeacher(teacher);
   }
 }
