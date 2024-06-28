5a77faffed4a58ce7ae6a16bc30afd6126d18a3a
Carlos ALvarez <carlosmail1978@gmail.com>
2024-06-28 14:16:27 +0200

    Update subject controller

--- src/main/java/com/example/goodToKnow/controller/SubjectController.java
+++ src/main/java/com/example/goodToKnow/controller/SubjectController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,16 +17,22 @@ import com.example.goodToKnow.service.SubjectService;
 
 @RestController()
 @RequestMapping(path = "api/v1/subjects")
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class SubjectController {
 
   @Autowired
   private SubjectService subjectService;
 
-  @CrossOrigin(origins = "http://localhost:4200")
   @GetMapping
   @ResponseStatus(HttpStatus.OK)
   public List<Subject> getSubjects() {
     return subjectService.findSubjects();
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Subject createSubject(@RequestBody Subject subject) {
+    return subjectService.saveSubject(subject);
   }
 
 }
